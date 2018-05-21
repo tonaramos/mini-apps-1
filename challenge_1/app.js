1abd9445d91a4fde214036e2abb72bc9e7c96514
Gabriel Tonatiuh Ramos Vilchis <Tona-@Macbook-Pro-2.local>
2018-05-21 16:43:02 -0700

    tic tac toe board added and cell content toggle implemented

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,36 @@
 
+console.log('hello');
+
+let xTurn = true; 
+let x = 'X';
+let o = 'O';
+let cells = {
+  a1: null,
+  a2: null,
+  a3: null,
+  b1: null,
+  b2: null,
+  b3: null,
+  c1: null,
+  c2: null,
+  c3: null,
+};
+
+
+let takeTurn = function (id) {
+ // console.log('you clicked target->', id);
+  if (xTurn && cells[id] === null) {
+    document.getElementById(id).innerHTML = 'X';
+    cells[id] = x;
+    //console.log(cells);
+    xTurn = !xTurn;
+  } else if (cells[id] === null) {
+    document.getElementById(id).innerHTML = 'O';
+    cells[id] = o;
+    //console.log(cells);
+    xTurn = !xTurn;
+  }
+};
+
+
 
