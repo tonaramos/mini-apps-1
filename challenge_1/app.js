643e8e60665bb7c4cb00876d7c01992134cc7f64
Gabriel Tonatiuh Ramos Vilchis <Tona-@Macbook-Pro-2.local>
2018-05-21 19:18:47 -0700

    Tie/GameOver outcome action, and listing of next turn added.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,7 +1,6 @@
 
-console.log('hello');
-
-let xTurn = true; 
+let xTurn = true;
+let tie = false;
 let x = 'X';
 let o = 'O';
 let cells = {
@@ -16,73 +15,80 @@ let cells = {
   c3: null,
 };
 
+let checkWinner = function() {
+  let scoreBoard = [
+    [cells.a1, cells.a2, cells.a3],
+    [cells.b1, cells.b2, cells.b3],
+    [cells.c1, cells.c2, cells.c3],
+    [cells.a1, cells.b1, cells.c1],
+    [cells.a2, cells.b2, cells.c2],
+    [cells.a3, cells.b3, cells.c3],
+    [cells.a1, cells.b2, cells.c3],
+    [cells.a3, cells.b2, cells.c1]
+  ];
+  
+  for (let i = 0; i < scoreBoard.length; i++) {
+    if (((scoreBoard[i][0] === scoreBoard[i][1]) && (scoreBoard[i][1] === scoreBoard[i][2])) 
+      && (scoreBoard[i][0] !== null && scoreBoard[i][1] !== null && scoreBoard[i][2] !== null)) {
+      alert('Player ' + scoreBoard[i][0] + ' wins!');
+      resetBoard();
+    };
+  };
+
+  let gameOver = true;
+
+  for (let j = 0; j < scoreBoard.length; j++) {
+    for (let k = 0; k < scoreBoard[j].length; k++) {
+      if (scoreBoard[j][k] === null) {
+        gameOver = false;
+        tie = !tie;
+      }
+    }
+  };  
+
+  if(gameOver === true && tie === true) {
+    alert('Is a tie, playAgain!');
+    resetBoard();
+  } else if ( gameOver === true ) {
+    alert('Game over, playAgain!');
+    resetBoard();
+  }
+};
+
+
+let resetBoard = function () {
+  let allCellElements = document.getElementsByTagName('td');
+  for (var i = 0; i < allCellElements.length; i++) {
+    allCellElements[i].innerHTML = '';
+  };
+  for (let key in cells){
+      cells[key] = null;
+  };
+};
 
 
 let takeTurn = function (id) {
   if (xTurn && cells[id] === null) {
     document.getElementById(id).innerHTML = 'X';
-    cells[id] = x
-    checkWinner();
+    cells[id] = x;
     xTurn = !xTurn;
   } else if (cells[id] === null) {
     document.getElementById(id).innerHTML = 'O';
     cells[id] = o;
-    checkWinner();
     xTurn = !xTurn;
   }
+  setTimeout(checkWinner, 1000);
+  nextPlayer();
 };
 
+let nextPlayer = function () {
+  let player = xTurn ? x : o ; 
+  document.getElementById('turnDisplay').innerHTML = 'Player ' + player + ' turn!';
+};
 
-let checkWinner = function() {
+nextPlayer();
 
-resetBoard();
 
-let scoreBoard = [
-[cells.a1, cells.a2, cells.a3],
-[cells.b1, cells.b2, cells.b3],
-[cells.c1, cells.c2, cells.c3],
-[cells.a1, cells.b1, cells.c1],
-[cells.a2, cells.b2, cells.c2],
-[cells.a3, cells.b3, cells.c3],
-[cells.a1, cells.b2, cells.c3],
-[cells.a3, cells.b2, cells.c1]
-];
-  
 
-  for (let i = 0; i < scoreBoard.length; i++) {
-    console.log('forlooprunning');
-    if (((scoreBoard[i][0] === scoreBoard[i][1]) && (scoreBoard[i][1] === scoreBoard[i][2])) 
-      && (scoreBoard[i][0] !== null && scoreBoard[i][1] !== null && scoreBoard[i][2] !== null)) {
-      console.log('Player ' + scoreBoard[i][0] + ' wins!');
-      return 'gameOver';
-    }
-  }
-  let gameOver = true;
-  for (let j = 0; j < scoreBoard.length; j++) {
-    for (let k = 0; k < scoreBoard[j].length; k++) {
-      if (scoreBoard[j][k] === null) {
-        gameOver = false;
-      }
-    }
-  }  
-  if(gameOver) {
-    console.log('gameOver, playAgain!')
-    for (let key in obj){
-      obj[key] = null;
-    }
-
-  }
-}
-
-let resetBoard = function () {
-
-  let allCellElements = document.getElementsByTagName('td');
-  console.log(allCellElements);
-
-  for (var i = 0; i < allCellElements.length; i++) {
-    allCellElements[i].innerHTML = 'Y';
-  }
-  
-}
 
 
