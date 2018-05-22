4d346d3d876b25f9e0517e27b788410f7a675889
Gabriel Tonatiuh Ramos Vilchis <Tona-@Macbook-Pro-2.local>
2018-05-21 17:21:08 -0700

    added functional winner-check function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -17,20 +17,55 @@ let cells = {
 };
 
 
+
 let takeTurn = function (id) {
- // console.log('you clicked target->', id);
   if (xTurn && cells[id] === null) {
     document.getElementById(id).innerHTML = 'X';
-    cells[id] = x;
-    //console.log(cells);
+    cells[id] = x
+    checkWinner();
     xTurn = !xTurn;
   } else if (cells[id] === null) {
     document.getElementById(id).innerHTML = 'O';
     cells[id] = o;
-    //console.log(cells);
+    checkWinner();
     xTurn = !xTurn;
   }
 };
 
+
+let checkWinner = function() {
+let scoreBoard = [
+[cells.a1, cells.a2, cells.a3],
+[cells.b1, cells.b2, cells.b3],
+[cells.c1, cells.c2, cells.c3],
+[cells.a1, cells.b1, cells.c1],
+[cells.a2, cells.b2, cells.c2],
+[cells.a3, cells.b3, cells.c3],
+[cells.a1, cells.b2, cells.c3],
+[cells.a3, cells.b2, cells.c1]
+];
+  console.log(scoreBoard);
+
+  for (let i = 0; i < scoreBoard.length; i++) {
+    console.log('forlooprunning');
+    if (((scoreBoard[i][0] === scoreBoard[i][1]) && (scoreBoard[i][1] === scoreBoard[i][2])) 
+      && (scoreBoard[i][0] !== null && scoreBoard[i][1] !== null && scoreBoard[i][2] !== null)) {
+      console.log('Player ' + scoreBoard[i][0] + ' wins!');
+      return 'gameOver';
+    }
+  }
+  let gameOver = true;
+  for (let j = 0; j < scoreBoard.length; j++) {
+    for (let k = 0; k < scoreBoard[j].length; k++) {
+      if (scoreBoard[j][k] === null) {
+        gameOver = false;
+      }
+    }
+  }  
+  if(gameOver) {
+    console.log('gameOver, playAgain!')
+  }
+ // console.log(scoreBoard)
+}
 
 
